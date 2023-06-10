1f58206ff3601c1e73c7bc5806f9bebc0542b5d3
Enrique Pablo Molinari <enrique.molinari@gmail.com>
2023-06-10 11:21:44 -0300

    correcciones menores

---
+++ pruebas.js
@@ -0,0 +1,9 @@
+/*console.log("Hola!");
+
+let bla = 10;
+console.log(typeof bla);
+bla = "Esto es un string";
+console.log(typeof bla);
+*/
+let b = "456cde";
+console.log(b.split("").reverse().join(""));
